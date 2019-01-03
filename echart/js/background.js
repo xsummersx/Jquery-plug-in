5046106f6bd5a9115b57100d54f678647503942a
xsummersx <605369747@qq.com>
2019-01-03 10:26:45 +0800

    imgunload

--- echart/js/background.js
+++ echart/js/background.js
@@ -1,31 +1,31 @@
 
-"use strict";
-
 var canvas = document.getElementById('canvas'),
   ctx = canvas.getContext('2d'),
   w = canvas.width = window.innerWidth,
   h = canvas.height = window.innerHeight,
-
+    
   hue = 217,
   stars = [],
   count = 0,
-  maxStars = 1500;
+  maxStars = 1400;
 
+// Thanks @jackrugile for the performance tip! http://codepen.io/jackrugile/pen/BjBGoM
+// Cache gradient
 var canvas2 = document.createElement('canvas'),
-  ctx2 = canvas2.getContext('2d');
-canvas2.width = 100;
-canvas2.height = 100;
-var half = canvas2.width / 2,
-  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-gradient2.addColorStop(0.025, '#fff');
-gradient2.addColorStop(0.075, 'hsl(' + hue + ', 50%, 33%)');
-gradient2.addColorStop(0.1, 'hsl(' + hue + ', 33%, 6%)');
-gradient2.addColorStop(1, 'transparent');
+    ctx2 = canvas2.getContext('2d');
+    canvas2.width = 100;
+    canvas2.height = 100;
+var half = canvas2.width/2,
+    gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+    gradient2.addColorStop(0.025, '#fff');
+    gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+    gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+    gradient2.addColorStop(1, 'transparent');
 
-ctx2.fillStyle = gradient2;
-ctx2.beginPath();
-ctx2.arc(half, half, half, 0, Math.PI * 2);
-ctx2.fill();
+    ctx2.fillStyle = gradient2;
+    ctx2.beginPath();
+    ctx2.arc(half, half, half, 0, Math.PI * 2);
+    ctx2.fill();
 
 // End cache
 
@@ -34,7 +34,7 @@ function random(min, max) {
     max = min;
     min = 0;
   }
-
+  
   if (min > max) {
     var hold = max;
     max = min;
@@ -44,16 +44,10 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function maxOrbit(x, y) {
-  var max = Math.max(x, y),
-    diameter = Math.round(Math.sqrt(max * max + max * max));
-  return diameter / 2;
-}
-
 var Star = function() {
 
-  this.orbitRadius = random(maxOrbit(w, h));
-  this.radius = random(60, this.orbitRadius) / 12;
+  this.orbitRadius = random(w / 2 - 50);
+  this.radius = random(100, this.orbitRadius) / 10;
   this.orbitX = w / 2;
   this.orbitY = h / 2;
   this.timePassed = random(0, maxStars);
@@ -65,9 +59,9 @@ var Star = function() {
 }
 
 Star.prototype.draw = function() {
-  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
-    y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-    twinkle = random(10);
+  var x = Math.sin(this.timePassed + 1) * this.orbitRadius + this.orbitX,
+      y = Math.cos(this.timePassed) * this.orbitRadius/2 + this.orbitY,
+      twinkle = random(10);
 
   if (twinkle === 1 && this.alpha > 0) {
     this.alpha -= 0.05;
@@ -76,7 +70,7 @@ Star.prototype.draw = function() {
   }
 
   ctx.globalAlpha = this.alpha;
-  ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
   this.timePassed += this.speed;
 }
 
@@ -85,16 +79,16 @@ for (var i = 0; i < maxStars; i++) {
 }
 
 function animation() {
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.globalAlpha = 0.8;
-  ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
-  ctx.fillRect(0, 0, w, h)
-
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.globalAlpha = 0.8;
+    ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
+    ctx.fillRect(0, 0, w, h)
+  
   ctx.globalCompositeOperation = 'lighter';
   for (var i = 1, l = stars.length; i < l; i++) {
     stars[i].draw();
-  };
-
+  };  
+  
   window.requestAnimationFrame(animation);
 }
 
